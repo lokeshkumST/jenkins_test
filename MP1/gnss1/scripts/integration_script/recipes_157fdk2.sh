9955cd651e103c9ea2c507ae58d7f641d365c49b
Lokesh Kumar <lokesh.kumar@st.com>
2024-01-12 15:32:40 +0530

    test environment variables for jenkins

--- MP1/gnss1/scripts/integration_script/recipes_157fdk2.sh
+++ MP1/gnss1/scripts/integration_script/recipes_157fdk2.sh
@@ -1,6 +1,9 @@
 echo "cp -r files/X-LINUX-GNSS1-V1.2.0 $YOCTO_BUILD_PATH_JENKINS/"
+echo "cp -r files/X-LINUX-GNSS1-V1.2.0 env.$YOCTO_BUILD_PATH_JENKINS/"
 echo "cd $YOCTO_TOP_PATH_JENKINS"
+echo "cd env.$YOCTO_TOP_PATH_JENKINS"
 echo "cp -r  $JENKINS_RECIPE_PATH ./layers/meta-st"
+echo "cp -r  env.$JENKINS_RECIPE_PATH ./layers/meta-st"
 
 #bitbake-layers add-layer layers/meta-st/meta-gnss1
 #echo 'IMAGE_INSTALL:append = "gnss1"' >> layers/meta-st/meta-st-openstlinux/conf/layer.conf
